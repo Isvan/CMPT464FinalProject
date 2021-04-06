2bc58feed56cb8346113669bf7c43094d1f50ce0
Vlad Ryzhov <devryzhov@gmail.com>
2021-04-05 18:37:24 -0700

    Add navigation by dataset indices

--- DatasetViewerApp.py
+++ DatasetViewerApp.py
@@ -7,11 +7,14 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please run with .obj numbers (example: \"DatasetViewerApp.py 1997 2110 2121\")")
     else:
-        indices = sys.argv[1:]
+        datasetIndices = sys.argv[1:]
         models = []
-        for index in indices:
-            parts = dt.getDatasetMeshParts(index)
+        for index in datasetIndices:
+            objIndex = dt.getDatasetMeshObjIndex(index)
+            parts = dt.getDatasetObjParts(objIndex)
             models.append(mpv.Model(parts))
+
         mpv.setModels(models)
         mpv.start()
+        
 
