66d868bf6fdc2c70dd674c7d6d7d40d865eeab87
g-dewar <49361260+g-dewar@users.noreply.github.com>
2021-04-14 13:13:25 -0700

    fixed normals being 'fixed' incorrectly
    trimesh.repair.fix_normals was incorrectly facing the normals on meshes that have large holes, which appear to mostly be seats and backs, so I've made it not 'fix' these parts

--- DatasetUtils.py
+++ DatasetUtils.py
@@ -8,6 +8,8 @@ from io import StringIO
 
 # Splits the legs or arm rests by overlaying a "cross" at the centroid
 # Produces the meaningful parts (whole leg, whole armrest, etc.) whenever possible
+
+
 def splitPartMesh(partMesh, partLabel):
     meshes = []
 
@@ -27,20 +29,25 @@ def splitPartMesh(partMesh, partLabel):
 
             # in we have 2 armrests or 2 legs only, we won't be able to split the sideRight and sideLeft furthermore
             # but if we can split, then there must be 4 legs or >2 arm rests
-            isSideRightSplitMesh = len(trimesh.intersections.mesh_plane(sideRight, (0, 0, 1), sideRight.centroid)) == 0
+            isSideRightSplitMesh = len(trimesh.intersections.mesh_plane(
+                sideRight, (0, 0, 1), sideRight.centroid)) == 0
             if not isSideRightSplitMesh:
                 meshes.append((sideRight, 'right'))
             else:
-                meshes.append((sideRight.slice_plane(sideRight.centroid, (0, 0, 1)), 'right'))
-                meshes.append((sideRight.slice_plane(sideRight.centroid, (0, 0, -1)), 'right'))
+                meshes.append((sideRight.slice_plane(
+                    sideRight.centroid, (0, 0, 1)), 'right'))
+                meshes.append((sideRight.slice_plane(
+                    sideRight.centroid, (0, 0, -1)), 'right'))
 
-            
-            isSideLeftSplitMesh = len(trimesh.intersections.mesh_plane(sideLeft, (0, 0, 1), sideLeft.centroid)) == 0
+            isSideLeftSplitMesh = len(trimesh.intersections.mesh_plane(
+                sideLeft, (0, 0, 1), sideLeft.centroid)) == 0
             if not isSideLeftSplitMesh:
                 meshes.append((sideLeft, 'left'))
             else:
-                meshes.append((sideLeft.slice_plane(sideLeft.centroid, (0, 0, 1)), 'left'))
-                meshes.append((sideLeft.slice_plane(sideLeft.centroid, (0, 0, -1)), 'left'))
+                meshes.append((sideLeft.slice_plane(
+                    sideLeft.centroid, (0, 0, 1)), 'left'))
+                meshes.append((sideLeft.slice_plane(
+                    sideLeft.centroid, (0, 0, -1)), 'left'))
 
     return meshes
 
@@ -193,14 +200,16 @@ def getDatasetObjParts(datasetIndex):
     for part in chairParts:
         if(len(chairParts[part]['text']) > 0):
             partTri = trimesh.load(
-                StringIO(chairParts[part]['text']), file_type='obj', force='mesh')
-            trimesh.repair.fix_normals(partTri, multibody=False)
+                StringIO(chairParts[part]['text']), file_type='obj')
+            if(part == 'leg' or part == 'arm rest'):
+                trimesh.repair.fix_normals(partTri)
 
-            meshes = splitPartMesh(partTri, part) #part is label
+            meshes = splitPartMesh(partTri, part)  # part is label
             for body, side in meshes:
-                    body.visual.face_colors = np.full(shape=[body.faces.shape[0], 4], fill_value=trimesh.visual.color.hex_to_rgba(part_colors[part]))
-                    partMesh = pyrender.Mesh.from_trimesh(body, smooth=False)
-                    parts.append((partMesh, side, part))
+                body.visual.face_colors = np.full(
+                    shape=[body.faces.shape[0], 4], fill_value=trimesh.visual.color.hex_to_rgba(part_colors[part]))
+                partMesh = pyrender.Mesh.from_trimesh(body, smooth=False)
+                parts.append((partMesh, side, part))
 
     return parts
 
